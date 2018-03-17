15d271aea40f52c5a0612bf7d57844ef4008915f
Craig Bruenger <craigbruenger@Craigs-MacBook-Pro.local>
2018-03-17 06:25:34 -0500

    Admin stuff finished

--- src/main/java/com/capstone/jobby/web/controller/AdminController.java
+++ src/main/java/com/capstone/jobby/web/controller/AdminController.java
@@ -26,20 +26,20 @@ public class AdminController {
     private CompanyService companyService;
     @Autowired
     private JobService jobService;
-    @Autowired
-    private CandidateSkillService candidateSkillService;
-    @Autowired
-    private CandidateTechSkillService candidateTechSkillService;
-    @Autowired
-    private DesiredCBSkillService desiredCBSkillService;
-    @Autowired
-    private DesiredTechSkillService desiredTechSkillService;
-    @Autowired
-    private MatchService matchService;
-    @Autowired
-    private ProfilePicService profilePicService;
-    @Autowired
-    private ResumeService resumeService;
+//    @Autowired
+//    private CandidateSkillService candidateSkillService;
+//    @Autowired
+//    private CandidateTechSkillService candidateTechSkillService;
+//    @Autowired
+//    private DesiredCBSkillService desiredCBSkillService;
+//    @Autowired
+//    private DesiredTechSkillService desiredTechSkillService;
+//    @Autowired
+//    private MatchService matchService;
+//    @Autowired
+//    private ProfilePicService profilePicService;
+//    @Autowired
+//    private ResumeService resumeService;
 
     // Administrator Dashboard
     @RequestMapping(value = "/auth/admin/adminDashboard")
@@ -60,6 +60,8 @@ public class AdminController {
     }
 
 
+
+
     /*************************************
      *                                   *
      *         Candidate Queries         *
@@ -71,6 +73,61 @@ public class AdminController {
         List<Candidate> candidates = candidateService.findAll();
         model.addAttribute("candidates", candidates);
         return "private/admin/findAllQuery";
+    }
+
+    @SuppressWarnings("unchecked")
+    @RequestMapping(value = "/auth/admin/adminDashboard/query/findCandidateById")
+    public String findCandidateById(Model model, @RequestParam String q) {
+        Candidate candidate = candidateService.findById(Long.parseLong(q));
+        model.addAttribute("candidate", candidate);
+        return "private/admin/findByIdQuery";
+    }
+
+    @SuppressWarnings("unchecked")
+    @RequestMapping(value = "/auth/admin/adminDashboard/query/findCandidateByEmail")
+    public String findCandidateByEmail(Model model, @RequestParam String q) {
+        Candidate candidate = candidateService.findByUsername(q);
+        model.addAttribute("candidate", candidate);
+        return "private/admin/findByEmailQuery";
+    }
+
+    @RequestMapping(value = "/auth/admin/adminDashboard/query/activateCandidateById", method = RequestMethod.POST)
+    public String activateCandidateById(@RequestParam String q, RedirectAttributes redirectAttributes) {
+        Candidate candidate = candidateService.findById(Long.parseLong(q));
+        if (!candidate.isEnabled()) {
+            candidate.setEnabled(true);
+            candidateService.save(candidate);
+            redirectAttributes.addFlashAttribute("flash", new FlashMessage("Candidate account activated!", FlashMessage.Status.SUCCESS));
+        } else {
+            redirectAttributes.addFlashAttribute("flash", new FlashMessage("Candidate account already activated...", FlashMessage.Status.FAILURE));
+        }
+        return "redirect:/auth/admin/adminDashboard";
+    }
+
+    @RequestMapping(value = "/auth/admin/adminDashboard/query/deactivateCandidateById", method = RequestMethod.POST)
+    public String deactivateCandidateById(@RequestParam String q, RedirectAttributes redirectAttributes) {
+        Candidate candidate = candidateService.findById(Long.parseLong(q));
+        if (candidate.isEnabled()) {
+            candidate.setEnabled(false);
+            candidateService.save(candidate);
+            redirectAttributes.addFlashAttribute("flash", new FlashMessage("Candidate account deactivated!", FlashMessage.Status.SUCCESS));
+        } else {
+            redirectAttributes.addFlashAttribute("flash", new FlashMessage("Candidate account already deactivated...", FlashMessage.Status.FAILURE));
+        }
+        return "redirect:/auth/admin/adminDashboard";
+    }
+
+
+        @RequestMapping(value = "/auth/admin/adminDashboard/query/deleteCandidateById", method = RequestMethod.POST)
+    public String deleteCandidateById(@RequestParam String q, RedirectAttributes redirectAttributes) {
+        Candidate candidate = candidateService.findById(Long.parseLong(q));
+        if (candidate != null) {
+            candidateService.delete(candidate);
+            redirectAttributes.addFlashAttribute("flash",new FlashMessage("Candidate deleted!", FlashMessage.Status.SUCCESS));
+        } else {
+            redirectAttributes.addFlashAttribute("flash",new FlashMessage("No such candidate...", FlashMessage.Status.FAILURE));
+        }
+        return "redirect:/auth/admin/adminDashboard";
     }
 
 
@@ -90,6 +147,76 @@ public class AdminController {
         return "private/admin/findAllQuery";
     }
 
+    @SuppressWarnings("unchecked")
+    @RequestMapping(value = "/auth/admin/adminDashboard/query/findCompanyById")
+    public String findCompanyById(Model model, @RequestParam String q) {
+        Company company = companyService.findById(Long.parseLong(q));
+        if (company != null) {
+            List<Job> jobs = jobService.findJobsByCompanyId(company.getId());
+            boolean hasJobs = jobs.size() > 0;
+            model.addAttribute("company", company);
+            model.addAttribute("hasJobs", hasJobs);
+            model.addAttribute("jobs", jobs);
+        } else {
+            model.addAttribute("company", null);
+        }
+        return "private/admin/findByIdQuery";
+    }
+
+    @SuppressWarnings("unchecked")
+    @RequestMapping(value = "/auth/admin/adminDashboard/query/findCompanyByEmail")
+    public String findCompanyByEmail(Model model, @RequestParam String q) {
+        Company company = companyService.findByUsername(q);
+        if (company != null) {
+            List<Job> jobs = jobService.findJobsByCompanyId(company.getId());
+            boolean hasJobs = jobs.size() > 0;
+            model.addAttribute("company", company);
+            model.addAttribute("hasJobs", hasJobs);
+            model.addAttribute("jobs", jobs);
+        } else {
+            model.addAttribute("company", null);
+        }
+        return "private/admin/findByEmailQuery";
+    }
+
+    @RequestMapping(value = "/auth/admin/adminDashboard/query/activateCompanyById", method = RequestMethod.POST)
+    public String activateCompanyById(@RequestParam String q, RedirectAttributes redirectAttributes) {
+        Company company = companyService.findById(Long.parseLong(q));
+        if (!company.isEnabled()) {
+            company.setEnabled(true);
+            companyService.save(company);
+            redirectAttributes.addFlashAttribute("flash", new FlashMessage("Company account activated!", FlashMessage.Status.SUCCESS));
+        } else {
+            redirectAttributes.addFlashAttribute("flash", new FlashMessage("Company account already activated...", FlashMessage.Status.FAILURE));
+        }
+        return "redirect:/auth/admin/adminDashboard";
+    }
+
+    @RequestMapping(value = "/auth/admin/adminDashboard/query/deactivateCompanyById", method = RequestMethod.POST)
+    public String deactivateCompanyById(@RequestParam String q, RedirectAttributes redirectAttributes) {
+        Company company = companyService.findById(Long.parseLong(q));
+        if (company.isEnabled()) {
+            company.setEnabled(false);
+            companyService.save(company);
+            redirectAttributes.addFlashAttribute("flash", new FlashMessage("Company account deactivated!", FlashMessage.Status.SUCCESS));
+        } else {
+            redirectAttributes.addFlashAttribute("flash", new FlashMessage("Company account already deactivated...", FlashMessage.Status.FAILURE));
+        }
+        return "redirect:/auth/admin/adminDashboard";
+    }
+
+    @RequestMapping(value = "/auth/admin/adminDashboard/query/deleteCompanyById", method = RequestMethod.POST)
+    public String deleteCompanyById(@RequestParam String q, RedirectAttributes redirectAttributes) {
+        Company company = companyService.findById(Long.parseLong(q));
+        if (company != null) {
+            companyService.delete(company);
+            redirectAttributes.addFlashAttribute("flash",new FlashMessage("Company deleted!", FlashMessage.Status.SUCCESS));
+        } else {
+            redirectAttributes.addFlashAttribute("flash",new FlashMessage("No such company...", FlashMessage.Status.FAILURE));
+        }
+        return "redirect:/auth/admin/adminDashboard";
+    }
+
 
 
 
@@ -104,6 +231,26 @@ public class AdminController {
         List<Job> jobs = jobService.findAll();
         model.addAttribute("jobs", jobs);
         return "private/admin/findAllQuery";
+    }
+
+    @SuppressWarnings("unchecked")
+    @RequestMapping(value = "/auth/admin/adminDashboard/query/findJobById")
+    public String findJobById(Model model, @RequestParam String q) {
+        Job job = jobService.findById(Long.parseLong(q));
+        model.addAttribute("job", job);
+        return "private/admin/findByIdQuery";
+    }
+
+    @RequestMapping(value = "/auth/admin/adminDashboard/query/deleteJobById", method = RequestMethod.POST)
+    public String deleteJobById(@RequestParam String q, RedirectAttributes redirectAttributes) {
+        Job job = jobService.findById(Long.parseLong(q));
+        if (job != null) {
+            jobService.delete(job);
+            redirectAttributes.addFlashAttribute("flash",new FlashMessage("Job deleted!", FlashMessage.Status.SUCCESS));
+        } else {
+            redirectAttributes.addFlashAttribute("flash",new FlashMessage("No such job...", FlashMessage.Status.FAILURE));
+        }
+        return "redirect:/auth/admin/adminDashboard";
     }
 
 
